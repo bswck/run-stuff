22cf7cb81309b5923c864675e721f1de83710524
Bartosz Sławecki <bartosz@ilikepython.com>
2025-11-08 13:10:36 +0100

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -1,9 +1,37 @@
-import linecache, pathlib, runpy
+import os
+import subprocess
+import sys
+import tempfile
+from contextlib import contextmanager
+startup_code = "def foo():\n    1/0\n"
 
-p = pathlib.Path("t.py")
-p.write_text("1 / 0")
-try:
-    linecache.updatecache(str(p))
-    runpy.run_path(str(p))
-finally:
-    p.unlink()
+
+@contextmanager
+def new_startup_env(*, code: str, histfile: str = ".pythonhist"):
+    """Create environment variables for a PYTHONSTARTUP script in a temporary directory."""
+    with tempfile.TemporaryDirectory() as tmpdir:
+        filename = os.path.join(tmpdir, "pythonstartup.py")
+        with open(filename, "w") as f:
+            f.write(os.linesep.join(code.splitlines()))
+        yield {"PYTHONSTARTUP": filename, "PYTHON_HISTORY": os.path.join(tmpdir, histfile)}
+
+
+with new_startup_env(code=startup_code) as startup_env:
+    p = subprocess.Popen(
+        [os.path.join(os.path.dirname(sys.executable), "<stdin>"), "-q", "-i"],
+        env=os.environ | startup_env,
+        executable=sys.executable,
+        text=True,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+    p.stdin.write("foo()")
+    p.stdin.close()
+    data = p.stdout.read()
+    p.stdout.close()
+    # try to cleanup the child so we don't appear to leak when running
+    # with regrtest -R.
+    p.wait()
+    subprocess._cleanup()
+    print(data)
